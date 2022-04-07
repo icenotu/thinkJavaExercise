94889feb8ae18ee1beec75c459012efff5c9322c
icenotu <icenotu@hotmail.com>
2022-04-08 00:32:08 +0800

    习题1-2

---
+++ src/ch01/Hello.java
@@ -0,0 +1,11 @@
+package ch01;
+
+/**
+ * 第一个java程序
+ */
+public class Hello{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("How are you?");
+    }
+}
